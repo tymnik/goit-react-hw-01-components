2846231d656203e17fb4b16a92b808d056756300
Veronika Timakova <tymnik@ukr.net>
2023-11-06 13:03:55 +0300

    feat: jsx + css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,1 +1,29 @@
-import data from '../friends.json';
+import React from 'react';
+import friendsData from '../json/friends.json';
+import './FriendList.css';
+
+const FriendList = () => {
+  return (
+    <ul className="friendList">
+      {friendsData.map((friend, index) => (
+        <FriendListItem key={index} item={friend} />
+      ))}
+    </ul>
+  );
+};
+
+const FriendListItem = ({ item }) => {
+  const { avatar, name, isOnline } = item;
+
+  return (
+    <li className="friendItem">
+      <span className={isOnline ? 'status online' : 'status offline'}>
+        {isOnline ? 'Online' : 'Offline'}
+      </span>
+      <img className="friendAvatar" src={avatar} alt="User avatar" width="48" />
+      <p className="friendName">{name}</p>
+    </li>
+  );
+};
+
+export default FriendList;
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,1 +1,37 @@
-import data from '../transactions.json';
+import React from 'react';
+import transactionsData from '../json/transactions.json';
+import './TransactionHistory.css';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className="transactionHistory">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactionsData.map((transaction, index) => (
+          <TableRaw key={index} item={transaction} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const TableRaw = ({ item }) => {
+  const { type, amount, currency } = item;
+
+  return (
+    <tr>
+      <td>{type}</td>
+      <td>{amount}</td>
+      <td>{currency}</td>
+    </tr>
+  );
+};
+
+export default TransactionHistory;
